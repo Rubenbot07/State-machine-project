908ff2877a066df174a7d5c881b6890256895997
rubenbot07 <rubenbot77@hotmail.com>
2025-04-25 09:52:31 -0500

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/State-machine-project',
   plugins: [react()],
   server: {
     watch: {
